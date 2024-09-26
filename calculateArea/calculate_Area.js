60b38102862f9bb5f1ddd32940cea75282c33ec3
Vivek kale <vivekkale9405@gmail.com>
2024-09-26 07:53:26 -0400

    message

---
+++ calculateArea/calculate_Area.js
@@ -0,0 +1,11 @@
+let length;
+let width;
+// Inside the above function, fetch values from user as input. For this you need to get values using document.getElementById .
+function calculateArea(){
+    length = parseFloat(document.getElementId('length'.value));
+    width = parseFloat(document.getElementById('width').value);
+    let area = length * width;
+
+    document.getElementById('result').innerText = 'The area of the rectangle is: ${area}';
+}
+
